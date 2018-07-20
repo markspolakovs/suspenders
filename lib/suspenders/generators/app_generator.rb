107426e6c9cd05b20f2b771d1e26693de209cc7a
Mike Burns <mburns@thoughtbot.com>
2018-07-20 13:58:03 -0400

    Introduce suspenders:json
    Tools for JSON parsing, serialization. So far all it does is install the
    `oj` gem.
    
    This comes from our guides, which has recommended the `oj` gem for all
    our projects. This kind of information should live in Suspenders instead
    of our guides, so lets pull it into here.
    
    Inspired by https://github.com/thoughtbot/guides/pull/509 .

--- lib/suspenders/generators/app_generator.rb
+++ lib/suspenders/generators/app_generator.rb
@@ -179,6 +179,7 @@ module Suspenders
 
     def generate_default
       run("spring stop")
+      generate("suspenders:json")
       generate("suspenders:static")
       generate("suspenders:stylesheet_base")
       generate("suspenders:testing")
